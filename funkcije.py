016587c5111fbfc805658d6fcf0d71f42cd61851
pluto13x <saramilosavljevic6@gmail.com>
2024-05-26 19:56:20 +0200

    prebacene funkcije i popravljene do reseta

---
+++ funkcije.py
@@ -0,0 +1,97 @@
+import pygame
+sqrt2 = 1.42
+
+def ispis(sc, boja, screen, screenWidth):
+    font = pygame.font.Font('assets/Minecraft.ttf', 64)
+    text = font.render(str(sc), True, boja)
+    textRect = text.get_rect(center=(screenWidth // 2, 50))
+    screen.blit(text, textRect)
+
+def highscoreIspis(hi, boja, screen):
+    font = pygame.font.Font('assets/Minecraft.ttf', 40)
+    text = font.render("HI: ", True, boja)
+    textRect = text.get_rect(center=(50, 40))
+    screen.blit(text, textRect)
+    text = font.render(str(hi), True, boja)
+    textRect = text.get_rect(midleft = (80, 40))
+    screen.blit(text, textRect)
+
+def gameOver(screen, screenWidth, screenHeight, boja):
+    font = pygame.font.Font('assets/Minecraft.ttf', 90)
+    text = font.render("GAME OVER", True, boja)
+    textRect = text.get_rect(center=(screenWidth / 2, screenHeight / 2 - 25))
+    screen.blit(text, textRect)
+
+    font = pygame.font.Font('assets/Minecraft.ttf', 40)
+    text = font.render("Press R to play again", True, boja)
+    textRect = text.get_rect(center=(screenWidth / 2, screenHeight / 2 + 50))
+    screen.blit(text, textRect)
+
+    #cvrle.vsp = 0
+    #cvrle.gravity = 0
+
+    return 0, 0
+
+def sudar(tica, cev):
+    res = False
+    r = tica.radius * sqrt2 / 2
+    c = pygame.Vector2(tica.x, tica.y)
+    vrh = pygame.Rect(cev.x - cev.b / 2, 0, cev.b, cev.a1)
+    pod = pygame.Rect(cev.x - cev.b / 2, cev.y + cev.a / 2, cev.b, cev.a2)
+    if c.x + r >= vrh.left and c.y - r <= vrh.bottom:
+        res = True
+    if c.x + r >= pod.left and c.y + r >= pod.top:
+        res = True
+    return res
+
+def crtanje(cvrle, screen, bojaPozadine):
+    screen.fill(bojaPozadine)
+    cvrle.draw(screen)
+
+def reset():
+    cvrle, skokf, skaci, rez, stubic, rand, rand2, hsp, ubrzanje
+
+    skokf = 0
+    cvrle.gravity = 0 
+    skaci = 1
+    cvrle.vsp = jump
+    cvrle.update(dt)
+    rez = 0
+    ubrzanje = 0
+    
+    cvrle = Ptica(150, screenHeight/2, jump, r, 0)
+
+    rand = random.randint(a, screenHeight - a)
+    rand2 = random.randint(a, screenHeight - a)
+    hsp = 0
+    stubic = [Stub(screenWidth + b / 2, rand, a, b, boja1, boja2), Stub(screenWidth + b / 2 + screenWidth / 2 + b / 2, rand2, a, b, boja1, boja2)]
+
+def oblak():
+    scale = 4
+
+    image = pygame.image.load('assets/oblak1.png')
+    SIZE = (64 * scale, 47 * scale)
+    image = pygame.transform.scale(image, SIZE)
+    screen.blit(image, (400, 60))
+
+    image = pygame.image.load('assets/oblak2.png')
+    SIZE = (32 * scale, 32 * scale)
+    image = pygame.transform.scale(image, SIZE)
+    screen.blit(image, (100, 100))
+
+    image = pygame.image.load('assets/oblak3.png')
+    SIZE = (32 * scale, 32 * scale)
+    image = pygame.transform.scale(image, SIZE)
+    screen.blit(image, (250, 300))
+    
+    image = pygame.image.load('assets/oblak4.png')
+    SIZE = (64 * scale, 64 * scale)
+    image = pygame.transform.scale(image, SIZE)
+    screen.blit(image, (300, 500))
+
+    image = pygame.image.load('assets/oblak5.png')
+    SIZE = (32 * scale, 32 * scale)
+    image = pygame.transform.scale(image, SIZE)
+    screen.blit(image, (50, 500))
+
+#endregion
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from funkcije import *
 from ptica import Ptica
 from stub import Stub
 from pygame import mixer
@@ -41,7 +42,6 @@ gameover = False
 crna = (0, 0, 0)
 nebo = (179, 230, 255)
 rez = 0
-sqrt2 = 1.42
 #endregion
  
 #region pticji atributi
@@ -70,102 +70,6 @@ stubic = [Stub(screenWidth + b / 2, rand, a, b, boja1, boja2), Stub(screenWidth 
 file = open("assets/high.txt","r+")
 high = (int)(file.read())
 file.close()
-#endregion
-
-#region metode
-def ispis(sc):
-    font = pygame.font.Font('assets/Minecraft.ttf', 64)
-    text = font.render(str(sc), True, crna)
-    textRect = text.get_rect(center=(screenWidth // 2, 50))
-    screen.blit(text, textRect)
-
-def highscoreIspis(hi):
-    font = pygame.font.Font('assets/Minecraft.ttf', 40)
-    text = font.render("HI: ", True, crna)
-    textRect = text.get_rect(center=(50, 40))
-    screen.blit(text, textRect)
-    text = font.render(str(hi), True, crna)
-    textRect = text.get_rect(midleft = (80, 40))
-    screen.blit(text, textRect)
-
-def gameOver():
-    global vsp
-    cvrle.vsp = 0
-    cvrle.gravity = 0
-    vsp = 0
-
-    font = pygame.font.Font('assets/Minecraft.ttf', 90)
-    text = font.render("GAME OVER", True, crna)
-    textRect = text.get_rect(center=(screenWidth / 2, screenHeight / 2 - 25))
-    screen.blit(text, textRect)
-
-    font = pygame.font.Font('assets/Minecraft.ttf', 40)
-    text = font.render("Press R to play again", True, crna)
-    textRect = text.get_rect(center=(screenWidth / 2, screenHeight / 2 + 50))
-    screen.blit(text, textRect)
-
-def sudar(tica, cev):
-    res = False
-    r = tica.radius * sqrt2 / 2
-    c = pygame.Vector2(tica.x, tica.y)
-    vrh = pygame.Rect(cev.x - cev.b / 2, 0, cev.b, cev.a1)
-    pod = pygame.Rect(cev.x - cev.b / 2, cev.y + cev.a / 2, cev.b, cev.a2)
-    if c.x + r >= vrh.left and c.y - r <= vrh.bottom:
-        res = True
-    if c.x + r >= pod.left and c.y + r >= pod.top:
-        res = True
-    return res
-
-def crtanje():
-    screen.fill(nebo)
-    cvrle.draw(screen)
-
-def reset():
-    global cvrle, skokf, skaci, rez, stubic, rand, rand2, hsp, ubrzanje
-
-    skokf = 0
-    cvrle.gravity = 0 
-    skaci = 1
-    cvrle.vsp = jump
-    cvrle.update(dt)
-    rez = 0
-    ubrzanje = 0
-    
-    cvrle = Ptica(150, screenHeight/2, jump, r, 0)
-
-    rand = random.randint(a, screenHeight - a)
-    rand2 = random.randint(a, screenHeight - a)
-    hsp = 0
-    stubic = [Stub(screenWidth + b / 2, rand, a, b, boja1, boja2), Stub(screenWidth + b / 2 + screenWidth / 2 + b / 2, rand2, a, b, boja1, boja2)]
-
-def oblak():
-    scale = 4
-
-    image = pygame.image.load('assets/oblak1.png')
-    SIZE = (64 * scale, 47 * scale)
-    image = pygame.transform.scale(image, SIZE)
-    screen.blit(image, (400, 60))
-
-    image = pygame.image.load('assets/oblak2.png')
-    SIZE = (32 * scale, 32 * scale)
-    image = pygame.transform.scale(image, SIZE)
-    screen.blit(image, (100, 100))
-
-    image = pygame.image.load('assets/oblak3.png')
-    SIZE = (32 * scale, 32 * scale)
-    image = pygame.transform.scale(image, SIZE)
-    screen.blit(image, (250, 300))
-    
-    image = pygame.image.load('assets/oblak4.png')
-    SIZE = (64 * scale, 64 * scale)
-    image = pygame.transform.scale(image, SIZE)
-    screen.blit(image, (300, 500))
-
-    image = pygame.image.load('assets/oblak5.png')
-    SIZE = (32 * scale, 32 * scale)
-    image = pygame.transform.scale(image, SIZE)
-    screen.blit(image, (50, 500))
-
 #endregion
 
 while running:
@@ -228,8 +132,8 @@ while running:
 
         cvrle.draw(screen)
         cvrle.gravitacija(dt) 
-        ispis(rez)
-        highscoreIspis(high)   
+        ispis(rez, crna, screen, screenWidth)
+        highscoreIspis(high, crna, screen)   
 
         if cvrle.pao():
             gameover = True
@@ -240,7 +144,7 @@ while running:
     #region gameover
     else:
         keys=pygame.key.get_pressed()
-        gameOver()
+        gameOver(screen, screenWidth, screenHeight)
         if keys[pygame.K_r]:
             reset()
             gameover = False
